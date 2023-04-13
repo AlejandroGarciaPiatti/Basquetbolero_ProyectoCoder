51add6337a4f692a5785168bb986992cdc865456
Alejandro Garcia Piatti <agarciapiatti@hotmail.com>
2023-04-13 20:45:13 -0300

    Se trabaja en pagina equipos para mejorar interfaz mobile

--- scripts/menu.js
+++ scripts/menu.js
@@ -11,3 +11,48 @@ menuCierre.addEventListener("click", ()=>{
     menu.style.opacity = 0;
     menu.style.zIndex = -5;
 })
+
+// Desplegable para los equipos
+
+const desplegable = document.querySelector(".contenedor-info");
+const desplegableInner = document.querySelector(".contenedor-info-inner");
+const desplegableCierre = document.querySelector(".contenedor-info-cierre")
+const infoHawks = document.querySelector(".equipo-hawks");
+const logoHawks = document.querySelector(".hawks");
+
+const infoCeltics = document.querySelector(".equipos-celtics");
+const logoCeltics = document.querySelector(".celtics");
+
+
+const infoHawksCopy = infoHawks.cloneNode(true);
+const infoCelticsCopy = infoCeltics.cloneNode(true);
+
+
+logoHawks.addEventListener("click", (evt)=>{
+    evt.preventDefault();
+    infoHawksCopy.classList.toggle("contenedor-info-estilo") 
+    desplegableInner.appendChild(infoHawksCopy);
+    desplegable.style.opacity = 100;
+    desplegable.style.zIndex = 20;
+})
+
+logoCeltics.addEventListener("click", (evt)=>{
+    evt.preventDefault();
+    infoCelticsCopy.classList.toggle("contenedor-info-estilo") 
+    desplegableInner.appendChild(infoCelticsCopy);
+    desplegable.style.opacity = 100;
+    desplegable.style.zIndex = 20;
+})
+
+desplegableCierre.addEventListener("click", ()=>{
+    desplegable.style.opacity = 0;
+    desplegable.style.zIndex = -5;
+    removeAllChildNodes(desplegableInner)
+})
+
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
